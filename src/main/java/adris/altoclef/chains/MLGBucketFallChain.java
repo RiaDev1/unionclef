9f9268f74c2a696beae7e615b4b9c89385d0df24
RiaDev1 <riadev1@proton.me>
2026-05-05 22:38:18 +0800

    feat: make minPearlFallDistance configurable via @set
    Replaces hardcoded MIN_PEARL_FALL_DISTANCE (3.0) with a settings field.
    Can be changed at runtime: @set minPearlFallDistance 2.0
    Default: 3.0 blocks.
    
    (cherry picked from commit c3921e17c311999bcf0754cc3db67cff3be94548)

--- src/main/java/adris/altoclef/chains/MLGBucketFallChain.java
+++ src/main/java/adris/altoclef/chains/MLGBucketFallChain.java
@@ -34,6 +34,7 @@ public class MLGBucketFallChain extends SingleTaskChain implements ITaskOverride
     private boolean doingChorusFruit = false;
     private BlockPos lastGroundBlockPos = null;
     private final TimerGame voidFallTimer = new TimerGame(0.25);
+    private double _fallStartY = Double.MAX_VALUE;
 
     public MLGBucketFallChain(TaskRunner runner) {
         super(runner);
@@ -54,9 +55,16 @@ public class MLGBucketFallChain extends SingleTaskChain implements ITaskOverride
         if (mod.getPlayer().isOnGround()) {
             lastGroundBlockPos = mod.getPlayer().getBlockPos();
             voidFallTimer.reset();
+            _fallStartY = Double.MAX_VALUE;
         } else if (isInHellHole(mod)) {
+            // Track fall start for minimum distance check
+            if (_fallStartY == Double.MAX_VALUE) {
+                _fallStartY = mod.getPlayer().getY();
+            }
             if (mod.getItemStorage().hasItem(Items.ENDER_PEARL) && lastGroundBlockPos != null) {
-                if (voidFallTimer.elapsed()) {
+                if (voidFallTimer.elapsed()
+                        && _fallStartY != Double.MAX_VALUE
+                        && (_fallStartY - mod.getPlayer().getY()) >= mod.getModSettings().getMinPearlFallDistance()) {
                     Optional<Entity> closestPlayer = mod.getEntityTracker().getClosestEntity(
                             mod.getPlayer().getPos(),
                             p -> pearlAllowable(mod, (PlayerEntity) p),
