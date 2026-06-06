c3ecb894a2cd282021e08e97598aba27da598f6f
RiaDev1 <riadev1@proton.me>
2026-06-07 03:06:18 +0800

    fix: ServerPlayerEntityMixin damage() signature for MC 1.21.11 (adds ServerWorld param)

--- src/main/java/adris/altoclef/mixins/ServerPlayerEntityMixin.java
+++ src/main/java/adris/altoclef/mixins/ServerPlayerEntityMixin.java
@@ -4,6 +4,9 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+//#if MC >= 12111
+//$$ import net.minecraft.server.world.ServerWorld;
+//#endif
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +19,13 @@ public abstract class ServerPlayerEntityMixin extends LivingEntity {
         super(entityType, world);
     }
 
+    //#if MC >= 12111
+    //$$ @Inject(method = "damage", at = @At("HEAD"))
+    //$$ private void onDamage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    //$$ }
+    //#else
     @Inject(method = "damage", at = @At("HEAD"))
     private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
     }
+    //#endif
 }
